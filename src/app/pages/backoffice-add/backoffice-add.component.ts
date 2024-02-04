08503fb3dafb3e27f98f1e662a8427a8b14185a6
Cynthia Poiny <cynthia.poiny@gmail.com>
2024-02-04 15:27:12 +0100

    creation of the router of the project and change the favicon

---
+++ src/app/pages/backoffice-add/backoffice-add.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-backoffice-add',
+  standalone: true,
+  imports: [],
+  templateUrl: './backoffice-add.component.html',
+  styleUrl: './backoffice-add.component.css'
+})
+export class BackofficeAddComponent {
+
+}
