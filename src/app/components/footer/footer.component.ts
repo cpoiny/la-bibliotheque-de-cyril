747c5998d99360f7e13b85f5731011747415aa13
Cynthia Poiny <cynthia.poiny@gmail.com>
2024-02-11 11:51:35 +0100

    form_footer done, TO DO send the message and email to Cyril*

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -21,27 +21,58 @@ export class FooterComponent implements OnInit {
 
   // Formulaire de contact
   formContact!: FormGroup;
+  isInvalidMessage: boolean = false;
+  isInvalidEmail: boolean = false;
+  errorMessage : string = "";
+  errorEmail : string = "";
 
   ngOnInit() {
 
     //Instanciation de mon formulaire
     this.formContact = new FormGroup({
       message: new FormControl("", [
-        Validators.required,
-        Validators.minLength(5)
+        Validators.required
       ]),
       email: new FormControl("", [
         Validators.required,
-        Validators.pattern("^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$")
+         Validators.pattern("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$")
       ])
     })
 
   }
 
-
-  onSubmit() {
-    console.log("mon formulaire", this.formContact);
+  displayErrorMessageForMessage(): void {
+    this.errorMessage = this.formContact.get('message')?.hasError('required') ? 'Veuillez écrire un message' : "";
+    if(this.errorMessage){
+      this.isInvalidMessage = true;
+    } else {
+      this.isInvalidMessage = false;
+    }
   }
 
+  displayErrorMessageForEmail(): void {
+    this.errorEmail = (this.formContact.get('email')?.hasError('required') || this.formContact.get('email')?.hasError('pattern')) ? 
+    'Une adresse email valide est requise' : "";
+    if(this.errorEmail){
+      this.isInvalidEmail = true;
+    } else {
+      this.isInvalidEmail = false;
+    }
+  }
+
+  //TO DO : Send the message and email to Cyril 
+  onSubmit() {
+    this.displayErrorMessageForMessage();
+    this.displayErrorMessageForEmail();
+    
+    this.resetForm();
+  }
+
+  resetForm(){
+    if(this.formContact.valid) {
+
+      this.formContact.reset();
+    }
+  }
 
 }
