ca369e7d7785b46606a060fbfdb2847bfd42cded
Cynthia Poiny <cynthia.poiny@gmail.com>
2024-02-11 19:27:17 +0100

    WIP HEADER

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { CommonModule, ViewportScroller } from '@angular/common';
 import { NavigationEnd, Router, RouterModule, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { PageHeaderComponent } from './components/page-header/page-header.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterModule, HeaderComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, RouterModule, HeaderComponent, FooterComponent, PageHeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
