2a551add10a0f591a9f2dc942db5ab04a409ecc3
Cynthia Poiny <cynthia.poiny@gmail.com>
2024-02-12 12:07:28 +0100

    Ajout header

--- src/app/pages/blog/cookies/cookies.component.ts
+++ src/app/pages/blog/cookies/cookies.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { ConfidentialiteComponent } from '../confidentialite/confidentialite.component';
-import { HomeComponent } from '../home/home.component';
 import { RouterLink } from '@angular/router';
+import { HeaderComponent } from '../../../components/header/header.component';
 
 @Component({
   selector: 'app-cookies',
   standalone: true,
-  imports: [RouterLink, ConfidentialiteComponent, HomeComponent],
+  imports: [RouterLink, ConfidentialiteComponent, HeaderComponent],
   templateUrl: './cookies.component.html',
   styleUrl: './cookies.component.css'
 })
