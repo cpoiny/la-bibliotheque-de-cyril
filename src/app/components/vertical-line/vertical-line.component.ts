99d50fdd0f5b1da91d4beaa926fd861b7726ab0e
Cynthia Poiny <cynthia.poiny@gmail.com>
2024-02-13 09:44:55 +0100

    vertical line components

---
+++ src/app/components/vertical-line/vertical-line.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vertical-line',
+  standalone: true,
+  imports: [],
+  templateUrl: './vertical-line.component.html',
+  styleUrl: './vertical-line.component.css'
+})
+export class VerticalLineComponent {
+
+}
--- src/app/pages/blog/home/home.component.ts
+++ src/app/pages/blog/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../../components/header/header.component';
 import { RouterLink } from '@angular/router';
+import { VerticalLineComponent } from '../../../components/vertical-line/vertical-line.component';
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent,RouterLink],
+  imports: [HeaderComponent,RouterLink,VerticalLineComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
