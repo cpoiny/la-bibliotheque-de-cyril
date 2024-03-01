91965b9f23f6fc5d4b6a195a8c5edce2b7e737a7
Cynthia Poiny <cynthia.poiny@gmail.com>
2024-03-01 16:46:53 +0100

    add router link

--- src/app/pages/backOffice/connexion/connexion.component.ts
+++ src/app/pages/backOffice/connexion/connexion.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderBoComponent } from '../../../components/backOffice/header-bo/header-bo.component';
 
 @Component({
   selector: 'app-connexion',
   standalone: true,
-  imports: [],
+  imports: [HeaderBoComponent],
   templateUrl: './connexion.component.html',
   styleUrl: './connexion.component.css'
 })
