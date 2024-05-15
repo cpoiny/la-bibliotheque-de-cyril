2a5e2a52f448600a2fbac3d61c787ec6fa9b4b72
cpoiny <cynthia.poiny@gmail.com>
2024-05-15 07:57:18 +0000

    Texte de la page home

--- src/app/components/blog/card-category/card-category.component.ts
+++ src/app/components/blog/card-category/card-category.component.ts
@@ -26,8 +26,8 @@ export class CardCategoryComponent {
   cardsCategory: Signal<ICategoryCard[]> = signal([
     {
       id: 1,
-      title: "Litterature",
-      text: "Corporis suscipit laboriosam, isi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate.",
+      title: "Littérature",
+      text: "Ici, vous trouverez mes retours sur mes lectures préférées...Principalement de la Littérature classique et européenne, mais je m'aventure parfois outre-atlantique et vers le moyen-orient.",
       img: "assets/illustration/cat-litterature.png",
       link:"/la_bibliotheque_de_cyril/publications/litterature",
       icon: "assets/icons/livre.png"
@@ -35,7 +35,7 @@ export class CardCategoryComponent {
     {
       id: 2,
       title: "Cinéma",
-      text: "Agnam aliquam quaerat voluptatem.nsequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit ess.",
+      text: "Ici, j'évoque les souvenirs et impressions laissées par les plus grandes scènes du cinéma, vous y naviguerez entre les merveilles 'made in nouvelle vague' et le cinéma d'auteur italien en passant par les chefs d'oeuvres américains.",
       img: "assets/illustration/cat-cinema.png",
       link:"/la_bibliotheque_de_cyril/publications/cinema",
      icon: "assets/icons/clap.png"
@@ -43,7 +43,7 @@ export class CardCategoryComponent {
     {
       id: 3,
       title: "Citations",
-      text: "Isi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit.",
+      text: "Ici, un aphorisme ou une citation suffisent à résumer une pensée, une idée bien mieux qu'un roman...",
       img: "assets/illustration/cat-citation.png",
       link:"/la_bibliotheque_de_cyril/publications/citations",
       icon: "assets/icons/citation.png"
